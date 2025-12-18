535223ecb110f63ba4b3f5164b1a2d3ff42f5146
Milan <8134milanrnw@gmail.com>
2025-12-18 12:30:37 +0530

    optimised

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:myweatherapp/core/api_requests.dart';
